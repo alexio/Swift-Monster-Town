40510c8877e1e1fae7b8e14383129179391e8c6c
Alexio <alexioamota@gmail.com>
2016-05-08 09:41:07 -0700

    Initial project structure

---
+++ MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Alexio Mota on 5/8/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
